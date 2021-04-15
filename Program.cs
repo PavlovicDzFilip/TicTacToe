af598da9e31ce69d70589263e45b0ba4c49fedb1
Pavlović Dž Filip <pavlovic.dz.filip@outlook.com>
2021-04-15 23:26:54 +0200

    Level 1 completed

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace TicTacToe
 {
@@ -7,80 +6,16 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            char[][] table = new char[3][];
-            for (int i = 0; i < 3; i++)
-            {
-                table[i] = new char[3];
-                for (int j = 0; j < 3; j++)
-                {
-                    table[i][j] = ' ';
-                }
-            }
-
-            bool gameOver = false;
-            bool isXNext = true;
-            while (!gameOver)
+            var game = new TicTacToe();
+            while (!game.IsGameOver)
             {
                 Console.WriteLine("Waiting for input");
                 var input = Console.ReadLine();
                 var inputX = int.Parse(input.Split(' ')[0]);
                 var inputY = int.Parse(input.Split(' ')[1]);
 
-                var nextSign = isXNext ? 'x' : 'o';
-                isXNext = !isXNext;
-
-                // assume input is always correct, and the field is not already taken
-                table[inputX][inputY] = nextSign;
-
-                // check is the game over
-                var firstDiagonal = true;
-                var secondDiagonal = true;
-
-                // check rows
-                for (int i = 0; i < 3 && !gameOver; i++)
-                {
-                    if (table[i].All(x => x == table[i][0]) && table[i][0] != ' ')
-                    {
-                        gameOver = true;
-                    }
-
-                    var columnIsSame = true;
-                    for (int j = 1; j < 3; j++)
-                    {
-                        if (table[j][i] != table[j - 1][i] || table[0][i] == ' ')
-                        {
-                            columnIsSame = false;
-                        }
-                    }
-
-                    gameOver = gameOver || columnIsSame;
-
-                    if (table[i][i] != table[0][0] || table[0][0] == ' ')
-                    {
-                        firstDiagonal = false;
-                    }
-
-                    if (table[i][2 - i] != table[0][2] || table[0][2] == ' ')
-                    {
-                        secondDiagonal = false;
-                    }
-                }
-
-                gameOver = gameOver || firstDiagonal || secondDiagonal;
-
-
-                // print the table
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        Console.Write(table[i][j]);
-                    }
-
-                    Console.WriteLine();
-                }
-
-                Console.WriteLine();
+                game.Input(inputX, inputY);
+                game.Print();
             }
 
             Console.WriteLine("Game over!");
---
+++ TicTacToe.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+
+namespace TicTacToe
+{
+    public class TicTacToe
+    {
+        private readonly char[][] _table;
+        private bool _isXNext;
+        public bool IsGameOver { get; private set; }
+
+        public TicTacToe()
+        {
+            _table = GetTable();
+            _isXNext = true;
+        }
+
+        private static char[][] GetTable()
+        {
+            char[][] table = new char[3][];
+            for (int i = 0; i < 3; i++)
+            {
+                table[i] = new char[3];
+                for (int j = 0; j < 3; j++)
+                {
+                    table[i][j] = ' ';
+                }
+            }
+
+            return table;
+        }
+
+        public void Input(int inputX, int inputY)
+        {
+            if (IsGameOver)
+            {
+                throw new InvalidOperationException("Game is already completed");
+            }
+
+            var nextSign = _isXNext ? 'x' : 'o';
+            _isXNext = !_isXNext;
+            
+            // assume input is always correct, and the field is not already taken
+            _table[inputX][inputY] = nextSign;
+            CheckIsGameOver();
+        }
+
+        private void CheckIsGameOver()
+        {
+            var gameOver = false;
+
+            var firstDiagonal = true;
+            var secondDiagonal = true;
+            var allFieldsTaken = true;
+
+            for (int i = 0; i < 3 && !gameOver; i++)
+            {
+                if (_table[i].All(x => x == _table[i][0]) && _table[i][0] != ' ')
+                {
+                    gameOver = true;
+                }
+
+                var columnIsSame = true;
+                for (int j = 1; j < 3; j++)
+                {
+                    if (_table[j][i] != _table[j - 1][i] || _table[0][i] == ' ')
+                    {
+                        columnIsSame = false;
+                    }
+                }
+
+                gameOver = gameOver || columnIsSame;
+
+                if (_table[i][i] != _table[0][0] || _table[0][0] == ' ')
+                {
+                    firstDiagonal = false;
+                }
+
+                if (_table[i][2 - i] != _table[0][2] || _table[0][2] == ' ')
+                {
+                    secondDiagonal = false;
+                }
+
+                allFieldsTaken = allFieldsTaken && _table[i].All(x => x == ' ');
+            }
+
+            gameOver = gameOver || firstDiagonal || secondDiagonal || allFieldsTaken;
+
+            IsGameOver = gameOver;
+        }
+
+        public void Print()
+        {
+            // print the table
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(_table[i][j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
